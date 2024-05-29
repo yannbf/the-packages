aad9c1b27cad50b0c8b64093cfb9af1f92934f4a
Yann Braga <yannbf@gmail.com>
2024-05-29 16:05:03 +0200

    wip

--- libs/sb-vitest-plugin/src/index.ts
+++ libs/sb-vitest-plugin/src/index.ts
@@ -13,8 +13,10 @@ export const storybookTest = (options?: Partial<Options>): Plugin => {
     enforce: 'pre',
     config(config: any) {
       config.test = config.test ?? {}
-      // add a prefix to the tests
-      config.test.name = 'storybook'
+      // add a prefix to the tests when in a workspace scenario
+      if (config.workspaceConfigPath) {
+        config.test.name = 'storybook'
+      }
       // enable isolate false by default for better performance
       config.test.isolate = config.test.isolate ?? false
       // enable globals so there's more compatibility with third party libraries e.g. vi-canvas-mock
